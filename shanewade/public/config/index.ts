efd6dae3d80ba8a750ab27fcb786ba7a68f05d20
Shane Wade <shane.au.wade@gmail.com>
2023-05-03 17:47:00 -0700

    adjusting config for server images

--- shanewade/public/config/index.ts
+++ shanewade/public/config/index.ts
@@ -1,5 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-// export const server = dev ? 'http://localhost:7777' : 'https://your_deployment.server.com'
-
-export const server = 'http://localhost:7777'
+export const server = dev ? 'http://localhost:7777' : 'https://www.shanewade.dev'
