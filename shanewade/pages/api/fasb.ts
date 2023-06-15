bbe99ad0c39be83590a78eefc9eff34ea49130ff
Shane Wade <shane.au.wade@gmail.com>
2023-06-15 12:21:18 -0700

    adding error handling to edge function to get error message

--- shanewade/pages/api/fasb.ts
+++ shanewade/pages/api/fasb.ts
@@ -1,5 +1,7 @@
 import { converse } from '../../fasb'
 
+import { NextRequest, NextResponse } from 'next/server'
+
 export const config = {
   runtime: 'edge',
   regions: 'pdx1', // optional
@@ -7,16 +9,22 @@ export const config = {
 
 export default async (request: Request) => {
   const { query } = await request.json()
-  const message_stream = await converse(query)
+  try {
+    const message_stream = await converse(query)
 
-  return new Response(message_stream, {
-    headers: new Headers({
-      // since we don't use browser's EventSource interface, specifying content-type is optional.
-      // the eventsource-parser library can handle the stream response as SSE, as long as the data format complies with SSE:
-      // https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#sending_events_from_the_server
+    return new Response(message_stream, {
+      headers: new Headers({
+        // since we don't use browser's EventSource interface, specifying content-type is optional.
+        // the eventsource-parser library can handle the stream response as SSE, as long as the data format complies with SSE:
+        // https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#sending_events_from_the_server
 
-      'Content-Type': 'text/event-stream',
-      'Cache-Control': 'no-cache',
-    }),
-  })
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+      }),
+    })
+  } catch (error) {
+    return new Response(String(error), {
+      status: 500,
+    })
+  }
 }
