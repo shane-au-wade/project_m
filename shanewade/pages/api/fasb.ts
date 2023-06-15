14806d2bc60a8d93e47f95cd5bdac84276333f96
Shane Wade <shane.au.wade@gmail.com>
2023-06-15 10:12:48 -0700

    removing complexity from api route

--- shanewade/pages/api/fasb.ts
+++ shanewade/pages/api/fasb.ts
@@ -1,32 +1,22 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// import type { NextApiRequest, NextApiResponse } from 'next'
 import { converse } from '../../fasb'
-
-type Data = {
-  message: string
-}
-
-// export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-//   if (req.method === 'POST') {
-//     // Process a POST request
-//     const { query } = req.body
-//     const message = await converse(query)
-//     res.status(200).json({
-//       message: message,
-//     })
-//   }
-// }
-
-import { NextRequest, NextResponse } from 'next/server'
 
 export const config = {
   runtime: 'edge',
   regions: 'pdx1', // optional
 }
 
-export default async (request: NextRequest) => {
+export default async (request: Request) => {
   const { query } = await request.json()
   const message_stream = await converse(query)
 
-  return new NextResponse(message_stream)
+  return new Response(message_stream, {
+    headers: new Headers({
+      // since we don't use browser's EventSource interface, specifying content-type is optional.
+      // the eventsource-parser library can handle the stream response as SSE, as long as the data format complies with SSE:
+      // https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#sending_events_from_the_server
+
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+    }),
+  })
 }
