0a7c864f83ba632eae2e63a1896190f71d706cb4
Shane Wade <shane@Shanes-MacBook-Pro.local>
2025-10-24 20:31:35 -0700

    init the shane wade v2 application;2;13~- init an atrso frontend project;2;13~-init a empty hono server

---
+++ shanewade-v2/web/astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react()],
+  site: "https://www.shanewade.dev",
+  vite: {
+    server: {
+      fs: {
+        allow: [
+          // Allow serving files from the current working directory
+          "../node_modules",
+          "./public",
+          "./src",
+        ],
+      },
+    },
+  },
+});
